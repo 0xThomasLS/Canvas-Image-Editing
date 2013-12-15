85865a3242db7f0c36969a72388cd01658b40814
Thomas Le Sciellour <thomas.lesciellour@gmail.com>
2013-12-15 14:02:14 +0100

    new greyLevel

--- Traitements/greyLevel.js
+++ Traitements/greyLevel.js
@@ -2,7 +2,7 @@ function greyLevel() {
 	for(var x=0; x < img.width; x++) {
 		for(var y=0; y < img.height; y++) {
 			var pixel = getPixel(imgData, x, y);
-			var gris = (pixel.R+pixel.G+pixel.B)/3;
+			var gris = (pixel.R*0.2175)+(pixel.G*0.7154)+(pixel.B*0.0721);
 
 			setPixel(imgData, x, y, gris, gris, gris, pixel.alpha);
 		}
